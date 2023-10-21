419159bbe3cd598c8505a303b68bfe529124cb41
Bruno Staine <brunostaine@hotmail.com>
2023-10-21 13:12:06 -0300

    configurando app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PaiComponent } from './components/pai/pai.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, PaiComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
